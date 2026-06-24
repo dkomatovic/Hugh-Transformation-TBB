5044dc6eb3ecaee8d128b29ddee93563ee58258b
dkomatovic <danicakomatovic@gmail.com>
2026-06-24 15:45:14 +0200

    Final commit

--- Hough Transformation/Common/LineVisualization.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Structures.h"
-
-Image DrawLines(const Image& img, std::vector<Line> lines,
-    unsigned char r, unsigned char g, unsigned char b);
--- Hough Transformation/Parallel/ParallelTransformation.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Structures.h"
-#include <tbb/concurrent_vector.h>
-
-Image Grayscale(const Image& img);
-Image SobelEdgeDetection(const Image& grayImage, int threshold);
-std::vector<std::vector<int>> HoughTransform(const Image& edgeImage);
-tbb::concurrent_vector<Line> DetectLines(const std::vector<std::vector<int>>& accumulator, int threshold);
